8f252992fd52e56845f977aaa029c6bb430a5a0b
SpookySaikiK <ly2cfe@gmail.com>
2023-10-29 13:58:29 +1000

    connected game to index

--- JS/game_test.js
+++ JS/game_test.js
@@ -1,27 +1,27 @@
 "use strict";
 
-let canWith= 1600;
-let canHight= 800;
+let CanvasWidth= 1200;
+let CanvasHeight= 700;
 function setup(){
     
-    createCanvas(canWith,canHight);
+    createCanvas(CanvasWidth,CanvasHeight);
 }
 
 function draw(){
-    let recWith= 200;
-    let recHight= 100;
-    let recCentwith= canWith/2-recWith/2;
-    let recCenhight= canHight/2-recHight/2;
-    let textWithcent= canWith/2-64
-    let textHightcent= canHight/2+32
+    let recWidth= 200;
+    let recHeight= 100;
+    let recCentWidth= CanvasWidth/2-recWidth/2;
+    let recCenHeight= CanvasHeight/2-recHeight/2;
+    let textWidthcent= CanvasWidth/2-64
+    let textHeightcent= CanvasHeight/2+32
     
     background(120,120,120);
     fill(255,255,255);
-    rect (recCentwith, recCenhight, recWith, recHight, 10);
+    rect (recCentWidth, recCenHeight, recWidth, recHeight, 10);
     fill(255,0,0);
     textSize(20);
     textStyle (BOLD);
-    text("MouseX + MouseY", textWithcent, textHightcent);
+    text("MouseX + MouseY", textWidthcent, textHeightcent);
   
 
 }
