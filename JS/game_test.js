95fa81325a0c416be9e23720ab2d7c06f46593d8
plasmar10 <omrhireubens@gmail.com>
2023-10-29 16:58:47 +1100

    added camra X controls

--- JS/game_test.js
+++ JS/game_test.js
@@ -1,6 +1,7 @@
 "use strict";
 let mothershiphome;
 let oceanBackground;
+let player
 
 function preload(){
     oceanBackground = loadImage("./assets/Ocean.gif")
@@ -14,6 +15,7 @@ function setup() {
 function draw() {
     tests()
     ocean()
+    testingdraw()
 }
 
 function tests() {
@@ -46,4 +48,69 @@ image
 
 function mothership() {
 mothershiphome = new Sprite(1000,700,100)
+
+camera.x = 1000
+camera.y = 800;
+
+player = new Sprite();
+	player.d = 80;
+	player.color = 'magenta';
+    camera.zoom = 0.5;
+
+
 }
+
+
+function testingdraw() {
+
+    camera.on();
+
+	for (let i = 0; i < 10; i++) {
+		fill(i * 20, 200, 200); // blue to gray
+		rect(-250 + i * 50, -250 + i * 100, 750, 50);
+	}
+	//player.moveTowards(mouse, 0.01);
+	player.draw();
+    if (kb.presses('a')) { 
+        camera.x = camera.x+10
+    }
+    if (kb.presses('d')) { 
+        camera.x = camera.x-10
+        }
+
+	camera.off();
+}
+
+
+// function setup() {
+// 	new Canvas(500, 240);
+// 	player = new Sprite();
+// 	player.d = 80;
+// 	player.color = 'magenta';
+
+
+// 	camera.zoom = 0.5;
+// }
+
+// function draw() {
+// 	background(0);
+
+// 	camera.on();
+
+// 	for (let i = 0; i < 10; i++) {
+// 		fill(i * 20, 200, 200); // blue to gray
+// 		rect(-250 + i * 50, -250 + i * 100, 750, 50);
+// 	}
+// 	player.moveTowards(mouse, 0.01);
+// 	player.draw();
+// 	camera.x = player.x;
+// 	camera.y = player.y;
+
+// 	camera.off();
+
+// 	ui.color = 'orange';
+// 	for (let i = 0; i < 9; i++) {
+// 		if (kb[i + 1]) ui[i].color = 'red';
+// 	}
+// 	ui.draw();
+// }
